78f67a4816a819539d4fc88cd558d66f029ff08c
george-shammar <george.gbenle@outlook.com>
2021-08-18 21:01:11 +0000

    Add validation tests

--- spec/models/tracker_spec.rb
+++ spec/models/tracker_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Tracker, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure Tracker model has a 1:m relationship with the Item model
+  it { should have_many(:items).dependent(:destroy) }
+  # Validation tests
+  # ensure columns title and patient are present before saving
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:patient) }
 end
