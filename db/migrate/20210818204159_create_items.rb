364d59880f6281fc115e02d550c8d82c72ec07fb
george-shammar <george.gbenle@outlook.com>
2021-08-18 20:42:49 +0000

    Create models

--- db/migrate/20210818204159_create_items.rb
+++ db/migrate/20210818204159_create_items.rb
@@ -1,8 +1,8 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-      t.int :blood_pressure
-      t.int :blood_glucose
+      t.integer :blood_pressure
+      t.integer :blood_sugar
       t.references :tracker, null: false, foreign_key: true
 
       t.timestamps
