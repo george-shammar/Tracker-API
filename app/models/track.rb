040a0b3739504be62e410df1cbeab032501e9e26
george-shammar <george.gbenle@outlook.com>
2021-08-19 12:00:06 +0000

    Add association and validation tests for Day model

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,5 @@
 class Track < ApplicationRecord
+  has_many :days, dependent: :destroy
+
+  validates_presence_of :title, :patient
 end
--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Day, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:tracker) }
+  
+  it { should validate_presence_of(:blood_pressure) }
+  it { should validate_presence_of(:blood_sugar) }
 end
