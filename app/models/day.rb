266306f3f14432de836389c461ad2ecfea001a85
george-shammar <george.gbenle@outlook.com>
2021-08-19 12:01:55 +0000

    Implement Day model

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,6 @@
 class Day < ApplicationRecord
   belongs_to :track
+
+  validates_presence_of :blood_pressure
+  validates_presence_of :blood_sugar
 end
--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Day, type: :model do
-  it { should belong_to(:tracker) }
+  it { should belong_to(:track) }
   
   it { should validate_presence_of(:blood_pressure) }
   it { should validate_presence_of(:blood_sugar) }
