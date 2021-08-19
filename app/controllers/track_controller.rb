83edf04413c9fccb92af1c958d287009e02e376f
george-shammar <george.gbenle@outlook.com>
2021-08-19 12:47:09 +0000

    Create Track controllers

--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -1,2 +1,43 @@
 class TrackController < ApplicationController
+    before_action :set_todo, only: [:show, :update, :destroy]
+
+  # GET /tracks
+  def index
+    @tracks = Track.all
+    json_response(@tracks)
+  end
+
+  # POST /tracks
+  def create
+    @track = Track.create!(track_params)
+    json_response(@track, :created)
+  end
+
+  # GET /tracks/:id
+  def show
+    json_response(@track)
+  end
+
+  # PUT /tracks/:id
+  def update
+    @track.update(track_params)
+    head :no_content
+  end
+
+  # DELETE /tracks/:id
+  def destroy
+    @track.destroy
+    head :no_content
+  end
+
+  private
+
+  def track_params
+    # whitelist params
+    params.permit(:title, :patient)
+  end
+
+  def set_track
+    @track = Track.find(params[:id])
+  end
 end
